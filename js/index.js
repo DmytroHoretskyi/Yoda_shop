b437030a28e34e390b7fe54de54620959d14e351
elecktrosheep <serwserw16@gmail.com>
2019-11-21 22:12:58 +0000

    final

--- js/index.js
+++ js/index.js
@@ -10,3 +10,22 @@ function getAjaxHtml() {
     xhr.open('get', 'information.html', true);
     xhr.send();
 }
+const countDownDate = new Date ("Jan 1, 6020 00:00:00").getTime();
+const intervalId = setInterval(function() {
+  const now = new Date().getTime();
+  let distance = countDownDate - now;
+    
+  const days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  const hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  const seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+  document.getElementById("days").innerHTML = days;
+  document.getElementById("hours").innerHTML = hours;
+  document.getElementById("minutes").innerHTML = minutes;
+  document.getElementById("seconds").innerHTML = seconds;
+ 
+  if (distance <= 0) {
+    clearInterval(intervalId);
+  }
+}, 1000);
