3b484891c9972eb9694e54b235d26027121700bc
elecktrosheep <serwserw16@gmail.com>
2019-11-22 15:42:33 +0000

    final_reviev

--- js/index.js
+++ js/index.js
@@ -1,4 +1,4 @@
-document.querySelector('.get-ajax-html').addEventListener('click', getAjaxHtml);
+document.querySelector('.about_me').addEventListener('click', getAjaxHtml);
 
 function getAjaxHtml() {
     const xhr = new XMLHttpRequest();
@@ -14,18 +14,30 @@ const countDownDate = new Date ("Jan 1, 6020 00:00:00").getTime();
 const intervalId = setInterval(function() {
   const now = new Date().getTime();
   let distance = countDownDate - now;
-    
   const days = Math.floor(distance / (1000 * 60 * 60 * 24));
   const hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
   const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
   const seconds = Math.floor((distance % (1000 * 60)) / 1000);
-
   document.getElementById("days").innerHTML = days;
   document.getElementById("hours").innerHTML = hours;
   document.getElementById("minutes").innerHTML = minutes;
   document.getElementById("seconds").innerHTML = seconds;
- 
   if (distance <= 0) {
     clearInterval(intervalId);
   }
 }, 1000);
+
+$('.info_button').click(() => {
+    $('.information_about_product').toggle(1000);
+});
+
+let el = document.getElementById('message_number');
+el.onkeypress = function(e) {   
+  let prohibited = "/[qwertyuiopaesdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM]/,";
+	let key = String.fromCharCode(e.which);  
+  if(prohibited.indexOf(key) >= 0){
+		console.log('invalid key pressed');    
+   	return false;
+  }
+  return true;    
+};
